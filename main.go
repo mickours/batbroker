3971e451fcc87a745c31208e1c4c4b81804a8125
MERCIER Michael <michael.mercier@inria.fr>
2017-12-07 17:15:20 +0100

    first working broker: only one HPC scheduler

--- main.go
+++ main.go
@@ -1,16 +1,117 @@
 package main
 
 import (
+	//zmq "github.com/pebbe/zmq4"
 	"gitlab.inria.fr/batsim/batbrocker/message"
-	"time"
+	//"time"
+	"encoding/json"
+	"fmt"
+	//"os"
+	"strings"
 )
 
+//func main() {
+//host := "localhost"
+//port := "5555"
+//req_sock := message.NewRequestSocket(host, port)
+//rep_sock := message.NewReplySocket(host, port)
+//go message.SendRequest(req_sock)
+//go message.SendReply(rep_sock)
+//time.Sleep(50 * time.Microsecond)
+//}
+
+type BatMessage struct {
+	Now    float64
+	Events []Event
+}
+
+type Event struct {
+	Timestamp float64
+	Type      string
+	Data      map[string]interface{}
+}
+
 func main() {
-	host := "localhost"
-	port := "5555"
-	req_sock := message.NewRequestSocket(host, port)
-	rep_sock := message.NewReplySocket(host, port)
-	go message.SendRequest(req_sock)
-	go message.SendReply(rep_sock)
-	time.Sleep(50 * time.Microsecond)
+	bat_host := "127.0.0.1"
+	bat_port := "28000"
+	bat_sock := message.NewReplySocket(bat_host, bat_port)
+	defer bat_sock.Close()
+
+	hpc_host := "127.0.0.1"
+	hpc_port := "28001"
+	hpc_sock := message.NewRequestSocket(hpc_host, hpc_port)
+	defer hpc_sock.Close()
+
+	//bda_host := "localhost"
+	//bda_port := "5557"
+	//bda_sock := message.NewRequestSocket(bda_host, bda_port)
+
+	jmsg := BatMessage{}
+	this_is_the_end := false
+	// main loop
+	for !this_is_the_end {
+		msg, err := bat_sock.RecvBytes(0)
+		if err != nil {
+			panic("Error while receiving Batsim message: " + err.Error())
+		}
+
+		if err := json.Unmarshal(msg, &jmsg); err != nil {
+			panic(err)
+		}
+		fmt.Println("Received batsim message:\n", jmsg)
+
+		for _, event := range jmsg.Events {
+			fmt.Println("BATSIM EVENT: ", event.Type)
+			switch event.Type {
+			case "JOB_SUBMITTED":
+				{
+					// get workload id
+					workload_id := strings.Split(event.Data["job_id"].(string), "!")[0]
+					fmt.Println("Workload id: ", workload_id)
+				}
+			case "SIMULATION_BEGINS":
+				{
+					fmt.Println("Hello!")
+					// TODO get workload/schdeduler mapping (Need batsim feature)
+				}
+			case "JOB_COMPLETED":
+				{
+					// get workload id
+					workload_id := strings.Split(event.Data["job_id"].(string), "!")[0]
+					fmt.Println("Workload id: ", workload_id)
+					// TODO manage HPC jobs epilog here
+				}
+			case "SIMULATION_ENDS":
+				{
+					fmt.Println("Bye Bye!")
+					this_is_the_end = true
+				}
+			}
+		}
+
+		fmt.Println("Forwarding Batsim message to HPC scheduler")
+		hpc_sock.SendBytes(msg, 0)
+		msg, err = hpc_sock.RecvBytes(0)
+		if err != nil {
+			panic("Error while receiving Batsim message: " + err.Error())
+		}
+
+		if err := json.Unmarshal(msg, &jmsg); err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Received HPC message:\n", jmsg)
+		for _, event := range jmsg.Events {
+			switch event.Type {
+			case "EXECUTE_JOB":
+				{
+					// TODO manage HPC job prolog here
+				}
+			default:
+				fmt.Println("Not Handled HPC EVENT: ", event.Type)
+			}
+		}
+		fmt.Println("Forward HPC message to Batsim:\n", jmsg)
+		bat_sock.SendBytes(msg, 0)
+	}
 }
--- message/zmq.go
+++ message/zmq.go
@@ -4,7 +4,6 @@ import (
 	zmq "github.com/pebbe/zmq4"
 
 	"fmt"
-	"time"
 )
 
 func newSocket(host string, port string, zmq_socket_type zmq.Type) *zmq.Socket {
@@ -14,7 +13,6 @@ func newSocket(host string, port string, zmq_socket_type zmq.Type) *zmq.Socket {
 	if err != nil {
 		panic("Unable to create the socket" + host + ":" + port + " with error: " + err.Error())
 	}
-	defer socket.Close()
 	return socket
 }
 
@@ -23,41 +21,19 @@ func newSocket(host string, port string, zmq_socket_type zmq.Type) *zmq.Socket {
 func NewRequestSocket(host string, port string) *zmq.Socket {
 	requester := newSocket(host, port, zmq.REQ)
 	// Connect to Reply socket
-	requester.Connect("tcp://" + host + ":" + port)
+	err := requester.Connect("tcp://" + host + ":" + port)
+	if err != nil {
+		panic("Error while connecting to socket: " + err.Error())
+	}
 	return requester
 }
 
 func NewReplySocket(host string, port string) *zmq.Socket {
 	responder := newSocket(host, port, zmq.REP)
-	responder.Bind("tcp://" + host + ":" + port)
+	endpoint := "tcp://" + host + ":" + port
+	err := responder.Bind(endpoint)
+	if err != nil {
+		panic("Error while binding socket to \"" + endpoint + "\" with error: " + err.Error())
+	}
 	return responder
-}
-
-func SendRequest(requester *zmq.Socket) {
-	for {
-		// send hello
-		msg := fmt.Sprintf("Hello %d", time.Now())
-		fmt.Println("Sending ", msg)
-		requester.Send(msg, 0)
-
-		// Wait for reply:
-		reply, _ := requester.Recv(0)
-		fmt.Println("Received ", reply)
-	}
-}
-
-func SendReply(responder *zmq.Socket) {
-	for {
-		//  Wait for next request from client
-		msg, _ := responder.Recv(0)
-		fmt.Println("Received ", msg)
-
-		//  Do some 'work'
-		time.Sleep(time.Second)
-
-		//  Send reply back to client
-		reply := "World"
-		responder.Send(reply, 0)
-		fmt.Println("Sent ", reply)
-	}
 }
